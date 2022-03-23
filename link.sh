c93398ca55ff8641b9816d156ef057e0d0445f36
Andrew Lamzed-Short <me@andyls.co.uk>
2022-03-23 11:13:26 +0000

    Linking script to tie my keymaps into the QMK folder for compilation
    This is just a more sane way of dealing with my custom keymaps as opposed to having to fork the entire QMK repo, forgetting where my keymaps are, my version being out-of-date etc.

---
+++ link.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -sfvn $HOME/projects/keymaps/preonic $HOME/qmk_firmware/keyboards/preonic/keymaps/andylshort
+ln -sfvn $HOME/projects/keymaps/ferris  $HOME/qmk_firmware/keyboards/ferris/keymaps/andylshort
